b7a0fc39d5692f0b7c2f633388eb7d94d49d4f9a
smike3 <smike@nm.ru>
2017-11-29 15:28:14 +0300

    Переработка с использованием фрагментов

---
+++ app/src/main/java/net/black/smike/cl/MPV.java
@@ -0,0 +1,228 @@
+package net.black.smike.cl;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+
+/**
+ * Created by smike on 28.11.17.
+ */
+
+public class MPV extends Fragment implements View.OnClickListener{
+
+    Button bt_mpv, bt_mpvstop,bt_mpv_up,bt_mpv_down,bt_mpv_quit;
+    TaskConn tk;
+    Pref pp=new Pref();
+    String mvp_files="";
+    private static final String TAG = "LogCL";
+    ServerSocket servers = null;
+    Socket fromclient = null;
+    private  static final int    serverPort = 3379;
+    private  static final String localhost  = "127.0.0.1";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.mpv, null);
+        bt_mpv=(Button)v.findViewById(R.id.bt_mvp2);
+        bt_mpv_up=(Button)v.findViewById(R.id.bt_mpv_up);
+        bt_mpv_down=(Button)v.findViewById(R.id.bt_mpv_down);
+        bt_mpv_quit=(Button)v.findViewById(R.id.bt_mpv_quit);
+        bt_mpvstop=(Button)v.findViewById(R.id.bt_mpvstop2);
+        bt_mpv.setOnClickListener(this);
+        bt_mpv_quit.setOnClickListener(this);
+        bt_mpv_up.setOnClickListener(this);
+        bt_mpv_down.setOnClickListener(this);
+        bt_mpvstop.setOnClickListener(this);
+        SharedPreferences prf;
+        Pref ppp=new Pref();
+        prf = this.getActivity().getSharedPreferences("CL_settings", Context.MODE_PRIVATE);
+        //String ip_s=prf.getString("server_name", "");
+        //System.out.println("\n!"+ip_s);
+        //return ip_s;
+        ppp.set_ipAddr(prf.getString("server_name", ""));
+        ppp.set_path_mvp(prf.getString("mvp_dir", ""));
+        pp=ppp;
+        tk=new TaskConn();
+        tk.execute("1dir"+pp.path_mvp,pp.ipAddr);
+        return v;
+    }
+
+
+    class TaskConn extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected String doInBackground(String... line) {
+            Socket socket = null;
+            String line2 = "";
+            int c;
+            try {
+                try {
+                    //byte[] ipAddr = new byte[]{10, 6, (byte)133, 66};
+
+                    System.out.println("\n!!!!" + line[1]);
+                    //InetAddress ipAddress = InetAddress.getByAddress(ipAddr);
+                    //InetAddress ipAddress = InetAddress.getByName("10.6.133.66");
+                    InetAddress ipAddress = InetAddress.getByName(line[1]);
+
+                    System.out.println("Welcome to Client side\n" +
+                            "Connecting to the server\n\t" +
+                            "(IP address " + localhost +
+                            ", port " + serverPort + ")");
+                    // InetAddress ipAddress = InetAddress.getByName(localhost);
+                    //socket = new Socket(ipAddress, serverPort);
+                    socket = new Socket();
+                    //socket.setSoTimeout(1000);
+                    socket.connect(new InetSocketAddress(line[1], serverPort), 1000);
+
+                    System.out.println("The connection is established.");
+
+                    System.out.println(
+                            "\tLocalPort = " +
+                                    socket.getLocalPort() +
+                                    "\n\tInetAddress.HostAddress = " +
+                                    socket.getInetAddress().getHostAddress() +
+                                    "\n\tReceiveBufferSize (SO_RCVBUF) = " +
+                                    socket.getReceiveBufferSize());
+
+                    // Получаем входной и выходной потоки сокета для обмена
+                    // сообщениями с сервером
+                    InputStream sin = socket.getInputStream();
+                    OutputStream sout = socket.getOutputStream();
+
+                    DataInputStream in = new DataInputStream(sin);
+                    DataOutputStream out = new DataOutputStream(sout);
+
+
+                    //    String line=et.getText().toString();
+                    //      tv.append(line);
+                    //   String line="sdfffd";
+                    //   out.writeUTF(line);     // Отсылаем строку серверу
+                    byte[] bb = line[0].getBytes();
+                    //   String comm="playback-play";
+                    //byte[] bb=line[0].getBytes();
+                    out.write(bb, 0, bb.length);
+                    out.flush();            // Завершаем поток
+                    //line2 = in.readUTF();    // Ждем ответа от сервера
+                    byte[] bb2 = new byte[20480];
+                    // Log.d(TAG,bb2.toString());
+                    c = in.read(bb2);
+                    //bb2[c]='\0';
+                    line2 = new String(bb2);
+                    line2 = line2.substring(0, c);
+                    //  Log.d(TAG,"!"+bb2.toString()+"!"+c);
+                    //  tv.append('\n'+line2);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.d(TAG, "Exce 1");
+                    line2="0[000]none";
+                    // return "0[000]none";
+
+                }
+            } finally {
+                try {
+                    if (socket != null)
+                        socket.close();
+                    Log.d(TAG, "Exce 2");
+
+                } catch (IOException e) {
+                    //e.printStackTrace();
+                    //                   return line2;
+                    Log.d(TAG, "Exce 3");
+                }
+            }
+            Log.d(TAG, "Exce 111");
+            return line2;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d(TAG, "123"+result);
+            // if(aud_data.vol==-1) aud_data.vol=Integer.parseInt(result.substring(1,4));
+            switch (Integer.parseInt(result.substring(0,1))) {
+                case 0:
+                    //if(aud_data.vol<=0) {
+              //      aud_data.vol = Integer.parseInt(result.substring(2, 5));
+             //       skb.setProgress(aud_data.vol);
+                    //}
+                    //else aud_data.vol = Integer.parseInt(result.substring(2, 5));
+               //     tv.setText(result.substring(6));
+              //      tv_vol.setText(Integer.toString(aud_data.vol));
+               //     System.out.println("!!!!!!!"+skb.getProgress());
+                    //tv_vol.setText(skb.getProgress());
+                    break;
+                case 1:
+                    //  String files[]=result.split("!");
+                    //    Log.d(TAG, files[0]);
+
+
+                    if(result.substring(1,4).equals("dir")) mvp_files = result.substring(5);
+                    break;
+            }
+            // tv.setText(result.substring(0,1));
+        }
+    }
+
+    public void onClick(View view) {
+            tk=new TaskConn();
+            switch(view.getId()) {
+                case R.id.bt_mpv_quit:
+                    tk.execute("1qui", pp.ipAddr);
+                    break;
+                case R.id.bt_mpv_up:
+                    tk.execute("1vup", pp.ipAddr);
+                    break;
+                case R.id.bt_mpv_down:
+                    tk.execute("1vdn", pp.ipAddr);
+                    break;
+                case R.id.bt_mvp2:
+                    //System.out.println(pp.);
+                    Intent intent_mvp = new Intent(getActivity().getApplication(), MVP_list.class);
+                    intent_mvp.putExtra("mvp_files",mvp_files);
+                    startActivityForResult(intent_mvp,1);
+                    break;
+                case R.id.bt_mpvstop2:
+                    System.out.println("########"+pp.ipAddr);
+                    tk.execute("1stp", pp.ipAddr);
+                    break;
+                default:
+                    //       tv.setText("22");
+                    break;
+            }
+        }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) return;
+        String name=data.getStringExtra("name_mvp");
+        System.out.println("1mvp"+pp.path_mvp+name);
+        tk=new TaskConn();
+        tk.execute("1mvp"+pp.path_mvp+name,pp.ipAddr);
+    }
+}
--- app/src/main/java/net/black/smike/cl/MainActivity.java
+++ app/src/main/java/net/black/smike/cl/MainActivity.java
@@ -1,11 +1,10 @@
 package net.black.smike.cl;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Message;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,80 +12,51 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
+class Aud_Data{
+    int vol;
+    void norm()
+    {
+        if(vol<0) vol=0;
+        if(vol>100) vol=100;
+    }
+    Aud_Data()
+    {
+        vol=-1;
+    }
+}
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.TimeUnit;
+class Pref{
+    String ipAddr,path_mvp;
+    public void set_ipAddr(String s)
+    {
+        ipAddr=s;
+    }
+    public void set_path_mvp(String s)
+    {
+        path_mvp=s+"/";
+    }
+}
 
-
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    int n_port = 6778;
-    TaskConn tk;
-    TextView tv;
-    TextView tv_vol;
-    Aud_Data aud_data=new Aud_Data();
-    SharedPreferences prf;
-    Pref pp=new Pref();
-
-//
-    Button bt_mvp,bt_mpvstop;
-    ImageButton bt_st,bt_ps,bt_adv,bt_rev,bt_pl;
-    SeekBar skb;
+public class MainActivity extends AppCompatActivity{
+    MPV frag_mpv;
+    Auda frag_auda;
+    FragmentTransaction ft;
+    FragmentManager fm;
     Toolbar tlb;
-    Handler handler;
-        //String str_info = "", ipAddr,path_mvp;
-    String str_info = "",mvp_files="";
-    ServerSocket servers = null;
-    Socket fromclient = null;
-    private static final String TAG = "LogCL";
-
-    private  static final int    serverPort = 3379;
-    private  static final String localhost  = "127.0.0.1";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bt_pl=(ImageButton)findViewById(R.id.bt_pl);
-        bt_st=(ImageButton)findViewById(R.id.bt_st);
-        bt_ps=(ImageButton)findViewById(R.id.bt_ps);
-        bt_adv=(ImageButton)findViewById(R.id.bt_adv);
-        bt_rev=(ImageButton)findViewById(R.id.bt_rev);
-        bt_mvp=(Button)findViewById(R.id.bt_mvp);
-        bt_mpvstop=(Button)findViewById(R.id.bt_mpvstop);
-        tv = (TextView)findViewById(R.id.tv);
-        tv_vol = (TextView)findViewById(R.id.tv_vol);
         tlb=(Toolbar)findViewById(R.id.tlb);
-        skb=(SeekBar) findViewById(R.id.skb);
-        bt_pl.setOnClickListener(this);
-        bt_st.setOnClickListener(this);
-        bt_ps.setOnClickListener(this);
-        bt_adv.setOnClickListener(this);
-        bt_rev.setOnClickListener(this);
-        bt_mvp.setOnClickListener(this);
-        bt_mpvstop.setOnClickListener(this);
-        skb.setOnSeekBarChangeListener(seekBarChangeListener);
+        frag_mpv = new MPV();
+        frag_auda= new Auda();
+        fm=getFragmentManager();
+        ft=fm.beginTransaction();
+        ft.add(R.id.laym, frag_auda);
+        ft.commit();
         setSupportActionBar(tlb);
         DrawerLayout dr_lay = (DrawerLayout) findViewById(R.id.dr_lay);
         ActionBarDrawerToggle tog = new ActionBarDrawerToggle(this, dr_lay, tlb,R.string.dr_open,R.string.dr_close);
@@ -100,11 +70,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public boolean onNavigationItemSelected(MenuItem item) {
                 switch(item.getItemId()){
                     case R.id.auda:
-
+                        ft=fm.beginTransaction();
+                        ft.replace(R.id.laym, frag_auda);
+                        ft.commit();
                         break;
                     case R.id.mpv:
-                        Intent intent = new Intent(getBaseContext(), Settings.class);
-                        startActivity(intent);
+                        ft=fm.beginTransaction();
+                        ft.replace(R.id.laym, frag_mpv);
+                        ft.commit();
                         break;
                     default:
                         return false;
@@ -116,29 +89,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         );
     }
-    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            aud_data.vol=skb.getProgress();
-            tk=new TaskConn();
-            tk.execute("0set-volume "+aud_data.vol,pp.ipAddr);
-        }
 
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-
-        }
-
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-
-        }
-    };
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.tlb_menu, menu);
         return true;
     }
@@ -164,242 +119,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             super.onBackPressed();
         }
     }
-    @Override
-    public void onClick(View v) {
-        tk=new TaskConn();
-        switch (v.getId()) {
-            case R.id.bt_adv:
-                tk.execute("0playlist-advance",pp.ipAddr);
-                break;
-            case R.id.bt_pl:
-                tk.execute("0playback-play",pp.ipAddr);
-                break;
-            case R.id.bt_st:
-                tk.execute("0playback-stop",pp.ipAddr);
-                break;
-            case R.id.bt_ps:
-                tk.execute("0playback-pause",pp.ipAddr);
-                break;
-            case R.id.bt_rev:
-                tk.execute("0playlist-reverse",pp.ipAddr);
-                break;
-            case R.id.bt_mvp:
-                Intent intent_mvp = new Intent(this, MVP_list.class);
-                intent_mvp.putExtra("mvp_files",mvp_files);
-                startActivityForResult(intent_mvp,1);
-                break;
-            case R.id.bt_mpvstop:
-                tk.execute("1stp",pp.ipAddr);
-                break;
-            default:
-                tv.setText("22");
-                break;
-
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (data == null) return;
-        String name=data.getStringExtra("name_mvp");
-        System.out.println("1mvp"+pp.path_mvp+name);
-        tk=new TaskConn();
-        tk.execute("1mvp"+pp.path_mvp+name,pp.ipAddr);
-    }
-
-    class Aud_Data{
-    int vol;
-        void norm()
-        {
-            if(vol<0) vol=0;
-            if(vol>100) vol=100;
-        }
-        Aud_Data()
-        {
-            vol=-1;
-        }
-    }
-
-    class MVP_Date{
 
 
-    }
-    class Pref{
-        String ipAddr,path_mvp;
-        public void set_ipAddr(String s)
-        {
-            ipAddr=s;
-        }
-        public void set_path_mvp(String s)
-        {
-            path_mvp=s+"/";
-        }
-    }
     Pref loadPref() {
+        SharedPreferences prf;
         Pref ppp=new Pref();
-        prf = getSharedPreferences("CL_settings",MODE_PRIVATE);
-        //String ip_s=prf.getString("server_name", "");
-        //System.out.println("\n!"+ip_s);
-        //return ip_s;
+        prf = getSharedPreferences("CL_settings", Context.MODE_PRIVATE);
         ppp.set_ipAddr(prf.getString("server_name", ""));
         ppp.set_path_mvp(prf.getString("mvp_dir", ""));
         return ppp;
-    }
-
-    class TaskConn extends AsyncTask<String, Void, String> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-        }
-
-        @Override
-        protected String doInBackground(String... line) {
-            Socket socket = null;
-            String line2 = "";
-            int c;
-            try {
-                try {
-                    //byte[] ipAddr = new byte[]{10, 6, (byte)133, 66};
-
-                    System.out.println("\n!!!!" + line[1]);
-                    //InetAddress ipAddress = InetAddress.getByAddress(ipAddr);
-                    //InetAddress ipAddress = InetAddress.getByName("10.6.133.66");
-                    InetAddress ipAddress = InetAddress.getByName(line[1]);
-
-                    System.out.println("Welcome to Client side\n" +
-                            "Connecting to the server\n\t" +
-                            "(IP address " + localhost +
-                            ", port " + serverPort + ")");
-                    // InetAddress ipAddress = InetAddress.getByName(localhost);
-                    //socket = new Socket(ipAddress, serverPort);
-                    socket = new Socket();
-                    //socket.setSoTimeout(1000);
-                    socket.connect(new InetSocketAddress(line[1], serverPort), 1000);
-
-                    System.out.println("The connection is established.");
-
-                    System.out.println(
-                            "\tLocalPort = " +
-                                    socket.getLocalPort() +
-                                    "\n\tInetAddress.HostAddress = " +
-                                    socket.getInetAddress().getHostAddress() +
-                                    "\n\tReceiveBufferSize (SO_RCVBUF) = " +
-                                    socket.getReceiveBufferSize());
-
-                    // Получаем входной и выходной потоки сокета для обмена
-                    // сообщениями с сервером
-                    InputStream sin = socket.getInputStream();
-                    OutputStream sout = socket.getOutputStream();
-
-                    DataInputStream in = new DataInputStream(sin);
-                    DataOutputStream out = new DataOutputStream(sout);
-
-
-                    //    String line=et.getText().toString();
-                    //      tv.append(line);
-                    //   String line="sdfffd";
-                    //   out.writeUTF(line);     // Отсылаем строку серверу
-                    byte[] bb = line[0].getBytes();
-                    //   String comm="playback-play";
-                    //byte[] bb=line[0].getBytes();
-                    out.write(bb, 0, bb.length);
-                    out.flush();            // Завершаем поток
-                    //line2 = in.readUTF();    // Ждем ответа от сервера
-                    byte[] bb2 = new byte[20480];
-                    // Log.d(TAG,bb2.toString());
-                    c = in.read(bb2);
-                    //bb2[c]='\0';
-                    line2 = new String(bb2);
-                    line2 = line2.substring(0, c);
-                    //  Log.d(TAG,"!"+bb2.toString()+"!"+c);
-                    //  tv.append('\n'+line2);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Log.d(TAG, "Exce 1");
-                    line2="0[000]none";
-                   // return "0[000]none";
-
-                }
-            } finally {
-                try {
-                    if (socket != null)
-                        socket.close();
-                    Log.d(TAG, "Exce 2");
-
-                } catch (IOException e) {
-                    //e.printStackTrace();
-                    //                   return line2;
-                    Log.d(TAG, "Exce 3");
-                }
-            }
-            Log.d(TAG, "Exce 111");
-            return line2;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            Log.d(TAG, "123"+result);
-            // if(aud_data.vol==-1) aud_data.vol=Integer.parseInt(result.substring(1,4));
-            switch (Integer.parseInt(result.substring(0,1))) {
-                case 0:
-                    if(aud_data.vol<0) {
-                        aud_data.vol = Integer.parseInt(result.substring(2, 5));
-                        skb.setProgress(aud_data.vol);
-                    }
-                    else aud_data.vol = Integer.parseInt(result.substring(2, 5));
-                    tv.setText(result.substring(6));
-                    tv_vol.setText(result.substring(2, 5));
-                    System.out.println("!!!!!!!"+skb.getProgress());
-                    //tv_vol.setText(skb.getProgress());
-                    break;
-                case 1:
-                  //  String files[]=result.split("!");
-                //    Log.d(TAG, files[0]);
-                 //   System.out.println(files.length);
-                    mvp_files=result.substring(5);
-                    break;
-            }
-           // tv.setText(result.substring(0,1));
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        pp=loadPref();
-        tk=new TaskConn();
-        tk.execute("0current-song",pp.ipAddr);
-        System.out.println("!----+"+aud_data.vol);
-
-        tk=new TaskConn();
-        tk.execute("1dir"+pp.path_mvp,pp.ipAddr);
-        Log.d(TAG, "MainActivity: onStart()");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "MainActivity: onResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "MainActivity: onPause()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "MainActivity: onStop()");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "MainActivity: onDestroy()");
     }
 
 }
